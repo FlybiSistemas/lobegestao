165b376887bbd0d8ec92c2862a206b872a49a7fd
Carlos <carlos@fastcorban.com.br>
2022-09-14 11:57:53 -0300

    Correção no dashboard e pesquisa por cnpj no dashboard de lançamento

--- app/Http/Livewire/Lancamentos/DashboardLancamentos.php
+++ app/Http/Livewire/Lancamentos/DashboardLancamentos.php
@@ -10,13 +10,18 @@ use Illuminate\Support\Collection;
 class DashboardLancamentos extends Base
 {
     public    $ano          = null;
+    public    $cnpj         = null;
     public    $empresa      = null;
     protected $listeners    = ['refreshDashboardLancamentos' => '$refresh'];
     private   $meses = [];
+    private   $anos = [];
 
-    public function mount()
+    private function montarMeses()
     {
-        $this->ano = Carbon::now()->format('Y');
+        if (!$this->ano) {
+            $this->ano = Carbon::now()->format('Y');
+        }
+        $this->meses = [];
         for ($i = 1; $i <= 12; $i++) {
             $this->meses[] = Carbon::now()->createFromFormat("d/m/Y", "01/{$i}/{$this->ano}");
         }
@@ -37,29 +42,38 @@ class DashboardLancamentos extends Base
     private function pegarLancamentosNaData(string $tipo, int $ano): Collection
     {
         $query = $this->builder();
-        $query = $query->where('tipo', $tipo);
+        $query = $query->join('empresas as e', 'lancamentos.empresa_id', '=', 'e.id');
+        $query = $query->select('lancamentos.*');
+        $query = $query->where('lancamentos.tipo', $tipo);
         $primeiroDia = Carbon::createFromFormat("d/m/Y", "01/01/{$ano}");
         $ultimoDia = Carbon::createFromFormat("d/m/Y", "31/12/{$ano}");
-        $query->whereBetween('data_lancamento', [$primeiroDia, $ultimoDia]);
+        $query = $query->whereBetween('lancamentos.data_lancamento', [$primeiroDia, $ultimoDia]);
+        if ($this->cnpj) {
+            $query = $query->where("e.cnpj", $this->cnpj);
+        }
 
         return $query->get();
     }
 
     public function lancamentosIcms()
     {
+        $this->montarMeses();
         $dadosDB = $this->pegarLancamentosNaData('M', $this->ano);
         $lancamentos = [];
+        // dd($this->meses, $this->ano);
         foreach ($this->meses as $mes) {
             $dadosMes = $dadosDB->where(fn ($item) => $item->data_lancamento->format('Y-m-d') == $mes->format('Y-m-d'))->first();
+            // dd($dadosMes);
             $lancamentoMes = [
                 "mes"               => $mes->format('m/Y'),
-                'data_lancamento'   => $mes->format('m/Y'),
+                'data_lancamento'   => $dadosMes ? $dadosMes->data_lancamento->format('m/Y') : $mes->format('m/Y'),
                 'valor_apurado'     => $dadosMes ? $dadosMes->valor_apurado : 0,
-                'valor_declarado'   => $dadosMes ? $dadosMes->valor_declardado : 0,
+                'valor_declarado'   => $dadosMes ? $dadosMes->valor_declarado : 0,
                 'valor_recolhido'   => $dadosMes ? $dadosMes->valor_recolhido : 0,
                 'resultado'         => $dadosMes ? $dadosMes->resultado : 0,
                 'tipo'              => 'M',
             ];
+            // dd($lancamentoMes, $dadosMes);
             $lancamentos[] = $lancamentoMes;
         }
         // dd($lancamentos);
@@ -76,7 +90,7 @@ class DashboardLancamentos extends Base
                 "mes"               => $mes->format('m/Y'),
                 'data_lancamento'   => $mes->format('m/Y'),
                 'valor_apurado'     => $dadosMes ? $dadosMes->valor_apurado : 0,
-                'valor_declarado'   => $dadosMes ? $dadosMes->valor_declardado : 0,
+                'valor_declarado'   => $dadosMes ? $dadosMes->valor_declarado : 0,
                 'valor_recolhido'   => $dadosMes ? $dadosMes->valor_recolhido : 0,
                 'resultado'         => $dadosMes ? $dadosMes->resultado : 0,
                 'tipo'              => 'M',
@@ -97,7 +111,7 @@ class DashboardLancamentos extends Base
                 "mes"               => $mes->format('m/Y'),
                 'data_lancamento'   => $mes->format('m/Y'),
                 'valor_apurado'     => $dadosMes ? $dadosMes->valor_apurado : 0,
-                'valor_declarado'   => $dadosMes ? $dadosMes->valor_declardado : 0,
+                'valor_declarado'   => $dadosMes ? $dadosMes->valor_declarado : 0,
                 'valor_recolhido'   => $dadosMes ? $dadosMes->valor_recolhido : 0,
                 'resultado'         => $dadosMes ? $dadosMes->resultado : 0,
                 'tipo'              => 'M',
